539666ed2241567191130794117a764c14da35cb
justinezahiri <justinezahiri@gmail.com>
2019-01-29 21:12:53 +0100

    implementation tribe creation WIP

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
 
+  //
+  //
+  //
+  //TODO: implement function to add another member 
+  // // let $members = $('.members')
+
+  // // $('#add-member').on('click', function () {
+  // let html = $('.members label').get(0).outerHTML;
+  // //   $members.append(html)
+  // // });
+
 }, false);
