bf6598ceee586a503b51e5f3f61a1f5b77b42fa0
“Archana” <archana.villalba@gmail.com>
2019-01-29 21:05:41 +0100

    signup done

--- routes/auth.js
+++ routes/auth.js
@@ -30,17 +30,23 @@ router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-  if(username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      message: "Indicate username and password"
+    });
     return;
   }
   const role = req.body.role;
   const color = req.body.color
 
   // create new user 
-  User.findOne({ email }, "email", (err, user) => {
-    if (email !== null) {
-      res.render("auth/signup", { message: "The email already exists" });
+  User.findOne({
+    email
+  }, "email", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        message: "The email already exists"
+      });
       return;
     }
 
@@ -56,58 +62,75 @@ router.post("/signup", (req, res, next) => {
     });
 
     newUser.save()
-    .then(() => {
-      // user is now persisted into DB, let's send him a welcome email
-      let { email, subject, message } = req.body;
-      let transporter = nodemailer.createTransport({
-        service: 'outlook',
-        auth: {
-          user: process.env.user,
-          pass: process.env.pass 
-        }
-      });
-      transporter.sendMail({
-        from: '"Justar 👻" <justar2019@outlook.fr>',
-        to: email, 
-        subject: "Welcome to My Tribe", 
-        text: welcome,
-        html: `<b>http://localhost:3000/auth/login</b>`
+      .then(() => {
+        // user is now persisted into DB, let's send him a welcome email
+        let {
+          email,
+          subject,
+          message
+        } = req.body;
+        let transporter = nodemailer.createTransport({
+          service: 'hotmail',
+          auth: {
+            user: 'justar2019@outlook.fr',
+            pass: 'ironhack75'
+          }
+        });
+        console.log(1)
+        transporter.sendMail({
+            from: '"Justar 👻" <justar2019@outlook.fr>',
+            to: email,
+            subject: "Welcome to My Tribe",
+            text: "welcome",
+            html: `<b>http://localhost:3000/auth/login</b>`
+          })
+          .then(message => {
+            console.log('ok email')
+            res.render('auth/message', {
+              email
+            })
+          })
+          .catch(error => {
+            console.log(error);
+            res.send('Nok email')
+          });
       })
-      .then(message=> {
-        res.send('ok email')
-        res.render('auth/message', {email})
+      .catch(err => {
+        console.log(err)
+        res.render("auth/signup", {
+          message: "Something went wrong"
+        });
       })
-      .catch(error => {
-        console.log(error);
-        res.send('Nok email')
-      });
-    })
-    .catch(err => {
-      res.send('nok save')
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
   });
 });
 
 router.post('/message', (req, res, next) => {
-  let { email, subject, message } = req.body;
+  let {
+    email,
+    subject,
+    message
+  } = req.body;
   let transporter = nodemailer.createTransport({
     service: 'outlook',
     auth: {
       user: 'justar2019',
-      pass: 'ironhack75' 
+      pass: 'ironhack75'
     }
   });
-    transporter.sendMail({
+  transporter.sendMail({
       from: '"Justar 👻" <justar2019@outlook.fr>',
-      to: email, 
-      subject: "Welcome to My Tribe", 
+      to: email,
+      subject: "Welcome to My Tribe",
       text: "Welcome to My Tribe",
       html: `<b>${message}</b>`
     })
-    .then(message=> res.render('auth/message', {email, subject, message}))
+    .then(message => res.render('auth/message', {
+      email,
+      subject,
+      message
+    }))
     .catch(error => console.log(error));
-  });
+});
 
 router.get("/logout", (req, res) => {
   req.logout();
