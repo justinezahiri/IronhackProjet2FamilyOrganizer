6806ab93b9d0ac4a28bffc5bbef0192fce1f5d19
unknown <laetitiaroche53@gmail.com>
2019-02-09 17:30:39 +0100

    Task toujours en cours

--- models/Tribe.js
+++ models/Tribe.js
@@ -1,9 +1,10 @@
-// const mongoose = require('mongoose');
-// const Schema   = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
 
 const tribeSchema = new Schema({
   name: String,
   members: [ { type: Schema.Types.ObjectId, ref: 'User' } ]
 });
-// const Tribe = mongoose.model('Tribe', tribeSchema);
-// module.exports = Tribe;
+
+const Tribe = mongoose.model('Tribe', tribeSchema);
+module.exports = Tribe;
